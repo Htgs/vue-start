871c0a452a019b644f904d0a48ff14f2b39f3ec0
bwx9394 <298057325@qq.com>
2017-02-23 18:49:14 +0800

    vue-router的学习。

--- test-vue/src/components/infomation.vue
+++ test-vue/src/components/infomation.vue
@@ -1,18 +1,8 @@
 <template>
-    <table>
-        <tr v-for="item in items">
-            <td>{{item.name}}</td>
-            <td>{{data}}</td>
-        </tr>
-    </table>
+
 </template>
 
 <script>
-
-export default {
-    props: [],
-    
-}
 
 </script>
 
