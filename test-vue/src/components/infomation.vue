f5a0b3dd91f93708a788da3239291183e0e78797
bwx9394 <298057325@qq.com>
2017-02-23 18:19:23 +0800

    update .gitignore

---
+++ test-vue/src/components/infomation.vue
@@ -0,0 +1,21 @@
+<template>
+    <table>
+        <tr v-for="item in items">
+            <td>{{item.name}}</td>
+            <td>{{data}}</td>
+        </tr>
+    </table>
+</template>
+
+<script>
+
+export default {
+    props: [],
+    
+}
+
+</script>
+
+<style>
+
+</style>
